6d8fd12a1c4da30f1f166b4c374169542a2dbdd3
brighthe98@gmail.com <brighthe98@gmail.com>
2023-12-15 10:29:11 +0800

    update

--- csm/linear_elasticity_lfem_example.py
+++ csm/linear_elasticity_lfem_example.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 from scipy.sparse.linalg import spsolve
 
@@ -9,6 +10,7 @@ from fealpy.functionspace import LagrangeFESpace as Space
 
 from fealpy.fem import LinearElasticityOperatorIntegrator
 from fealpy.fem import VectorSourceIntegrator
+from fealpy.fem import VectorMassIntegrator
 from fealpy.fem import BilinearForm
 from fealpy.fem import LinearForm
 from fealpy.fem import DirichletBC
@@ -54,22 +56,17 @@ mu = pde.mu
 lambda_ = pde.lam
 domain = pde.domain()
 mesh = pde.delaunay_mesh()
-import matplotlib.pyplot as plt
-fig = plt.figure()
-axes = fig.gca()
-mesh.add_plot(axes)
-mesh.find_cell(axes, showindex=True, color='k', marker='s', markersize=2, fontsize=8, fontcolor='k')
-mesh.find_node(axes, showindex=True, color='r', marker='o', markersize=2, fontsize=8, fontcolor='r')
-plt.show()
-
+#import matplotlib.pyplot as plt
+#fig = plt.figure()
+#axes = fig.gca()
+#mesh.add_plot(axes)
+#mesh.find_cell(axes, showindex=True, color='k', marker='s', markersize=2, fontsize=8, fontcolor='k')
+#mesh.find_node(axes, showindex=True, color='r', marker='o', markersize=2, fontsize=8, fontcolor='r')
+#plt.show()
 NN = mesh.number_of_nodes()
 NC = mesh.number_of_cells()
 node = mesh.entity('node')
 cell = mesh.entity('cell')
-print("mu:", mu)
-print("lambda:", lambda_)
-print("NN:", NN)
-print("NC:", NC)
 
 output = './mesh/'
 if not os.path.exists(output):
@@ -87,30 +84,62 @@ vldof = ldof * GD
 print("vgdof", vgdof)
 print("vldof", vldof)
 
-integrator1 = LinearElasticityOperatorIntegrator(lam=lambda_, mu=mu, q=4)
+integrator1 = LinearElasticityOperatorIntegrator(lam=lambda_, mu=mu, q=p+1)
 
 bform = BilinearForm(vspace)
 bform.add_domain_integrator(integrator1)
 KK = integrator1.assembly_cell_matrix(space=vspace)
-print("KK:", KK.shape)
-print("KK_19:\n", KK[19])
-print("KK_36:\n", KK[36])
+print("KK", KK.shape)
 bform.assembly()
 K = bform.get_matrix()
 print("K:", K.shape)
-print(K.toarray().round(4))
-#
-#integrator2 = VectorSourceIntegrator(f = pde.source)
-#
-#lform = LinearForm(vspace)
-#lform.add_domain_integrator(integrator2)
-#FK = integrator2.assembly_cell_vector(space = vspace)
-#print("FK:", FK.shape)
-## print("FK:", FK)
-#F = lform.assembly()
-#print(F.shape)
-## print("F:", F)
-#
+
+integrator2 = VectorMassIntegrator(c=1, q=5)
+
+bform2 = BilinearForm(vspace)
+bform2.add_domain_integrator(integrator2)
+MK = integrator2.assembly_cell_matrix(space=vspace)
+print("MK:", MK.shape)
+bform2.assembly()
+M = bform2.get_matrix()
+print("M:", M.shape)
+
+integrator3 = VectorSourceIntegrator(f = pde.source, q=5)
+
+lform = LinearForm(vspace)
+lform.add_domain_integrator(integrator3)
+FK = integrator3.assembly_cell_vector(space = vspace)
+print("FK[0]:", FK.shape, "\n", FK[0])
+lform.assembly()
+F = lform.get_vector()
+print("F:", F.shape, "\n", F.round(4))
+
+ipoints = space.interpolation_points()
+print("ipoints:", ipoints.shape)
+fh = pde.source(p=ipoints)
+print("fh:", fh.shape)
+fh_1 = np.zeros(M.shape[0])
+print("fh_1:", fh_1.shape)
+fh_1[::GD] = fh[:,0]
+fh_1[1::GD] = fh[:,1]
+Fh = M @ fh_1
+print("Fh:", Fh.shape, "\n", Fh.round(4))
+
+print("error:", np.sum(np.abs(F - Fh)))
+
+
+#node = mesh.entity('node')
+#f = pde.source(p=node)
+#print(f.shape)
+#f1 = np.zeros(M.shape[0])
+#print(f1.shape)
+#f1[::2] = f[:,0]
+#f1[1::2] = f[:,1]
+#F2 = M @ f1
+#print("F2:", F2.shape, "\n", F2.round(4))
+
+#print("error:", np.sum(np.abs(F-F2)))
+
 #if hasattr(pde, 'dirichlet'):
 #    bc = DirichletBC(space=vspace, gD=pde.dirichlet, threshold=pde.is_dirichlet_boundary)
 #    K, F = bc.apply(K, F, uh)
