f52cd2d923d9eebd343c73d8101058190181b87a
brighthe98@gmail.com <brighthe98@gmail.com>
2024-05-08 11:30:05 +0800

    update

--- mfdm/poisson_mfdm_primal.py
+++ mfdm/poisson_mfdm_primal.py
@@ -8,10 +8,9 @@ pde = SinSinData()
 #pde = SinSin5Data()
 #pde = CosCos5Data()
 ns = 2
-#mesh = pde.polygon_mesh()
 mesh = pde.polygon_mesh_2(n=ns)
 
-maxit = 1
+maxit = 5
 errorType = ['$|| p - p_h ||_{\\Omega,0}$']
 errorMatrix = np.zeros((2, maxit), dtype=np.float64)
 nDof = np.zeros(maxit, dtype=np.int_)
@@ -25,31 +24,30 @@ for iter in range(maxit):
     print("NN:", NN)
     eDdof = mesh.ds.boundary_edge_index()
     nDdof = mesh.entity('edge')[eDdof][:, 0]
-    #print("nDdof:", nDdof.shape, "\n", nDdof)
 
     solver = Mimetic(mesh)
 
-    MV, t1, error = solver.gmv()
-    #print("MV:", MV.shape, "\n", MV.round(3))
+    MV = solver.gmv()
+    print("MV:", MV.shape, "\n", MV)
 
-    ME, t2, error = solver.gme()
+    ME = solver.gme()
+    print("ME:", ME.shape, "\n", ME)
 
     grad_h = solver.grad_operator()
 
     A = grad_h.T @ ME @ grad_h
-    #print("A:", A.shape, "\n", A.round(3))
 
     node = mesh.entity('node')
     p = pde.solution(node)
-    print("p:", p.shape, "\n", p.round(3))
 
     node = mesh.entity('node')
     rhs = pde.source(node)
     ph = np.zeros(NN)
+
     ph[nDdof] = pde.solution(node[nDdof])
     b = MV @ rhs
     b = b - A @ ph
-    #print("b:", b.shape, "\n", b.round(3))
+    print("b:", b.shape, "\n", b)
 
     bdIdx = np.zeros(A.shape[0], dtype=np.int_)
     bdIdx[nDdof.flat] = 1
@@ -57,18 +55,33 @@ for iter in range(maxit):
     D0 = spdiags(1-bdIdx, 0, A.shape[0], A.shape[0]).toarray()
     D1 = spdiags(bdIdx, 0, A.shape[0], A.shape[0]).toarray()
     A = D0 @ A @ D0 + D1
-    #print("A:", A.shape, "\n", A.round(3))
+    print("A:", A.shape, "\n", A)
 
 
     b[nDdof] = pde.Dirichlet(node[nDdof])
-    #print("b:", b.shape, "\n", b.round(3))
+    print("b:", b.shape, "\n", b.round(3))
+
+    #f = pde.source(node)
+    #b = MV@f
+
+    #eDdof = mesh.ds.boundary_edge_index()
+    #nDdof = mesh.entity('edge')[eDdof][:, 0]
+    #b[nDdof] = pde.solution(node[nDdof])
+
+    #bdIdx = np.zeros(A.shape[0], dtype=np.int_)
+    #bdIdx[nDdof.flat] = 1
+    #from scipy.sparse import spdiags
+    #D0 = spdiags(1-bdIdx, 0, A.shape[0], A.shape[0]).toarray()
+    #D1 = spdiags(bdIdx, 0, A.shape[0], A.shape[0]).toarray()
+    #A = D0 @ A + D1
+
 
     ph = np.linalg.solve(A, b)
     print("ph:", ph.shape, "\n", ph.round(3))
 
 
-    errorMatrix[0, iter] = np.sum(np.abs(p - ph))
-    errorMatrix[1, iter] = np.sum(np.abs(p[nDdof] - ph[nDdof]))
+    errorMatrix[0, iter] = np.sum(np.abs(p - ph)) / NN
+    errorMatrix[1, iter] = np.sum(np.abs(p[nDdof] - ph[nDdof])) / NN
 
     if iter < maxit-1:
         print("iter:", iter)
