c356159db291638b63f0dcb95a28743c75530d05
brighthe98@gmail.com <brighthe98@gmail.com>
2024-05-26 14:36:38 +0800

    update

--- to/simp/top99/Benchmark_mbb_beam.m
+++ to/simp/top99/Benchmark_mbb_beam.m
@@ -7,31 +7,23 @@ rmin = 1.5;
 x(1:nely, 1:nelx) = volfrac;
 loop = 0;
 change = 1.;
+
+% % 打开一个文件用于写入
+% fileID = fopen('results.txt', 'w');
+% % 写入标题
+% fprintf(fileID, 'Iteration\tObjective\tVolume\tChange\n');
+
+% 创建一个视频写入对象
+v = VideoWriter('topology_optimization.avi');
+v.FrameRate = 10; % 设置帧率
+open(v);
+
 % Start Iteration
 while change > 0.01
     loop = loop + 1;
     xold = x;
     % FE-Analysis
     [U] = FE(nelx, nely, x, penal);
-    
-    % K = sparse(2*(nelx+1)*(nely+1), 2*(nelx+1)*(nely+1));
-    % F = sparse(2*(nely+1)*(nelx+1), 1); U = zeros(2*(nely+1)*(nelx+1), 1);
-    % for elx = 1:nelx
-    %     for ely = 1:nely
-    %         n1 = (nely+1)*(elx-1)+ely;
-    %         n2 = (nely+1)* elx   +ely;
-    %         edof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1; 2*n2+2; 2*n1+1; 2*n1+2];
-    %         K(edof, edof) = K(edof, edof) + x(ely, elx)^penal*KE;
-    %     end
-    % end
-    % % Define Loads And Suppotrs (Half-MBB-Beam)
-    % F(2, 1) = -1;
-    % fixeddofs = union([1:2:2*(nely+1)], [2*(nelx+1)*(nely+1)]);
-    % alldofs = [1:2*(nely+1)*(nelx+1)];
-    % freedofs = setdiff(alldofs, fixeddofs);
-    % % SOLVING
-    % U(freedofs,:) = K(freedofs,freedofs) \ F(freedofs,:);
-    % U(fixeddofs,:)= 0;
 
     % OBJECTIVE FUNCTION AND SENSITIVITY ANALYSIS
     [KE] = lk;
@@ -54,8 +46,19 @@ while change > 0.01
     disp([' It.: ' sprintf('%4i',loop) ' Obj.: ' sprintf('%10.4f',c) ...
         ' Vol.: ' sprintf('%6.3f',sum(sum(x))/(nelx*nely)) ...
         ' ch.: ' sprintf('%6.3f',change )])
+
+    % % 保存结果到文件
+    % fprintf(fileID, '%4i\t%10.4f\t%6.3f\t%6.3f\n', loop, c, sum(sum(x))/(nelx*nely), change);
+    
     % Plot Densities
     colormap(gray); imagesc(-x); axis equal; axis tight; axis off;pause(1e-6);
+
+    % 捕捉当前帧并写入视频
+    frame = getframe(gcf);
+    writeVideo(v, frame);
 end
 
-    
+% % 关闭文件
+% fclose(fileID);
+% 关闭视频写入对象
+close(v);
