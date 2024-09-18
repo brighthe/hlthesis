f647f0ba044ab6e9976001e5b7f99b31a229be38
brighthe98@gmail.com <brighthe98@gmail.com>
2024-09-18 16:19:47 +0800

    update

--- topoOpt/beso/top172/E2N.m
+++
@@ -1,7 +0,0 @@
-function [dN] = E2N(t, p, x, Ve)
-    dN = zeros(length(p), 1);
-    for i = 1:length(p)
-        [row, ~] = find(t==i);
-        dN(i) = dot(Ve(row), x(row)) / sum(Ve(row));
-    end
-end
--- topoOpt/simp/top3d/myConstrFcn.m
+++
@@ -1,9 +0,0 @@
-function [cneq, ceq, gradc, gradceq] = myConstrFcn(x, H, Hs, volfrac, nele)
-    xPhys(:) = (H*x(:))./Hs;
-    % Non-linear Constraints
-    cneq  = sum(xPhys(:)) - volfrac*nele;
-    gradc = ones(nele,1);
-    % Linear Constraints
-    ceq     = [];
-    gradceq = [];
-end % mycon
