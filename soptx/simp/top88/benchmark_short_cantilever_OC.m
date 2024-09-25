f4b589cd1e9ea6a235655e0a71f5cf02f073625d
brighthe98@gmail.com <brighthe98@gmail.com>
2024-09-25 11:32:20 +0800

    update

--- soptx/simp/top88/benchmark_short_cantilever_OC.m
+++ soptx/simp/top88/benchmark_short_cantilever_OC.m
@@ -1,9 +1,9 @@
-nelx = 4;
-nely = 3;
-volfrac = 0.5;
+nelx = 160;
+nely = 100;
+volfrac = 0.4;
 penal = 3;
-rmin = 1.5;
-ft = 2;
+rmin = 6;
+ft = 1;
 
 %% MATERIAL PROPERTIES
 E0 = 1;
@@ -19,12 +19,13 @@ KE = 1/(1-nu^2)/24*([A11 A12;A12' A11]+nu*[B11 B12;B12' B11]);
 nodenrs = reshape(1:(1+nelx)*(1+nely),1+nely,1+nelx);
 edofVec = reshape(2*nodenrs(1:end-1,1:end-1)+1,nelx*nely,1);
 edofMat = repmat(edofVec,1,8)+repmat([0 1 2*nely+[2 3 0 1] -2 -1],nelx*nely,1);
-iK = reshape(kron(edofMat,ones(8,1))',64*nelx*nely,1);
-jK = reshape(kron(edofMat,ones(1,8))',64*nelx*nely,1);
-% DEFINE LOADS AND SUPPORTS (HALF MBB-BEAM)
-F = sparse(2,1,-1,2*(nely+1)*(nelx+1),1);
+iK = reshape(kron(edofMat, ones(8,1))', 64*nelx*nely, 1);
+jK = reshape(kron(edofMat, ones(1,8))', 64*nelx*nely, 1);
+
+%% DEFINE LOADS AND SUPPORTS (Short Cantilever Beam)
+F = sparse(2*(nely+1)*(nelx+1), 1, -1, 2*(nely+1)*(nelx+1), 1);
 U = zeros(2*(nely+1)*(nelx+1),1);
-fixeddofs = union([1:2:2*(nely+1)], [2*(nelx+1)*(nely+1)]);
+fixeddofs = [1:2*nely+1];
 alldofs = [1:2*(nely+1)*(nelx+1)];
 freedofs = setdiff(alldofs,fixeddofs);
 
@@ -42,47 +43,36 @@ for i1 = 1:nelx
         k = k+1;
         iH(k) = e1;
         jH(k) = e2;
-        sH(k) = max(0,rmin-sqrt((i1-i2)^2+(j1-j2)^2));
+        sH(k) = max(0, rmin - sqrt((i1-i2)^2 + (j1 - j2)^2));
       end
     end
   end
 end
-H = sparse(iH,jH,sH);
-Hs = sum(H,2);
+H = sparse(iH, jH, sH);
+Hs = sum(H, 2);
 
 %% INITIALIZE ITERATION
 x = repmat(volfrac,nely,nelx);
-% x(1) = 0.8;
-% x(12) = 1.5;
 xPhys = x;
 loop = 0;
 change = 1;
 
-fileID = fopen('half_mbb.txt', 'w');
-fprintf(fileID, 'Iteration\tObjective\tVolume\tChange\n');
-v = VideoWriter('half_mbb.avi');
-v.FrameRate = 10;
-open(v);
 
 %% START ITERATION
 while change > 0.01
   loop = loop + 1;
 
   %% FE-ANALYSIS
-  E = (Emin+xPhys(:)'.^penal*(E0-Emin));
-  sK = reshape(KE(:)*(Emin+xPhys(:)'.^penal*(E0-Emin)), 64*nelx*nely,1);
-  K = sparse(iK, jK, sK); K = (K + K') / 2;
-  FullK = full(K(freedofs,freedofs));
-  FullF = F(freedofs);
-  U(freedofs) = K(freedofs,freedofs) \ F(freedofs);
+  sK = reshape(KE(:)*(Emin+xPhys(:)'.^penal*(E0-Emin)),64*nelx*nely,1);
+  K = sparse(iK,jK,sK); K = (K+K')/2;
+  U(freedofs) = K(freedofs,freedofs)\F(freedofs);
 
   %% OBJECTIVE FUNCTION AND SENSITIVITY ANALYSIS
-  Ue = U(edofMat);
   ce = reshape(sum((U(edofMat)*KE).*U(edofMat),2), nely, nelx);
   c = sum(sum((Emin + xPhys.^penal * (E0 - Emin)).*ce));
-   dc = -penal*(E0-Emin)*xPhys.^(penal-1).*ce;
+  dc = -penal*(E0-Emin)*xPhys.^(penal-1).*ce;
   dv = ones(nely, nelx);
-  
+
   %% FILTERING/MODIFICATION OF SENSITIVITIES
   if ft == 1
     dc(:) = H*(x(:).*dc(:))./Hs./max(1e-3, x(:));
@@ -90,31 +80,24 @@ while change > 0.01
     dc(:) = H*(dc(:)./Hs);
     dv(:) = H*(dv(:)./Hs);
   end
+
   %% OPTIMALITY CRITERIA UPDATE OF DESIGN VARIABLES AND PHYSICAL DENSITIES
   l1 = 0; l2 = 1e9; move = 0.2;
   while (l2-l1)/(l1+l2) > 1e-3
     lmid = 0.5*(l2+l1);
-    xnew = max(0,max(x-move,min(1,min(x+move,x.*sqrt(-dc./dv/lmid)))));
+    xnew = max(0,max(x-move, min(1,min(x+move, x.*sqrt(-dc./dv/lmid)))));
     if ft == 1
       xPhys = xnew;
     elseif ft == 2
       xPhys(:) = (H*xnew(:))./Hs;
     end
-	g = sum(xPhys(:)) - volfrac*nelx*nely;
-	if g > 0, l1 = lmid; else l2 = lmid; end
-    % if sum(xPhys(:)) > volfrac*nelx*nely, l1 = lmid; else l2 = lmid; end
+    if sum(xPhys(:)) > volfrac*nelx*nely, l1 = lmid; else l2 = lmid; end
   end
-  change = max(abs(xnew(:)-x(:)));
+  change = max(abs(xnew(:) - x(:)));
   x = xnew;
 
   %% PRINT RESULTS
-  fprintf(' It.:%5i Obj.:%11.4f Vol.:%7.3f ch.:%7.3f\n',loop,c, ...
-    mean(xPhys(:)),change);
-  %% 保存结果到文件
-  fprintf(fileID, '%4i\t%10.4f\t%6.3f\t%6.3f\n', loop, c, mean(xPhys(:)), change);
+  fprintf(' It.:%5i Obj.:%11.4f Vol.:%7.3f ch.:%7.3f\n', loop, c, mean(xPhys(:)),change);
   %% PLOT DENSITIES
   colormap(gray); imagesc(1-xPhys); caxis([0 1]); axis equal; axis off; drawnow;
-  %% 捕捉当前帧并写入视频
-  frame = getframe(gcf);
-  writeVideo(v, frame);
 end
